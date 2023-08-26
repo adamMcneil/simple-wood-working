2d080a9c8f6fcbcf8db3ac9df54dffbcf39e7bc8
Adam <adamwmcneil@gmail.com>
2023-08-26 18:52:03 -0500

    did some wierd stuff with vite

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,19 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false,
-			strict: true
-		})
-	}
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter({
+      base: process.env.NODE_ENV === "production" ? "/simple_wood_working" : "",
+      pages: "build",
+      assets: "build",
+      fallback: null,
+      precompress: false,
+      strict: true,
+    }),
+  },
 };
 
 export default config;
