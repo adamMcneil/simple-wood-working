2375fa97153a2ce941428d717a2a492643430a41
Adam <adamwmcneil@gmail.com>
2023-08-27 17:31:34 -0500

    added base to product links

--- src/routes/ProductLink.svelte
+++ src/routes/ProductLink.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div class="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col">
-    <a href={`/products/${product.slug}`}>
+    <a href={`/products/${process.env.NODE_ENV + product.slug}`}>
         <img
             class="hover:grow hover:shadow-lg h-full w-full object-cover aspect-square"
             src={product.image}
