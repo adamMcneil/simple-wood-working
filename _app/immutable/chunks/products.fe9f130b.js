b8d3e66f553e5f7c9d43ee26a80f0c322759fdfb
adamwm4 <adamwm4@illinois.edu>
2025-06-20 13:01:36 -0500

    bens table

---
+++ _app/immutable/chunks/products.fe9f130b.js
@@ -0,0 +1,71 @@
+var c=Object.defineProperty;var g=(t,e,i)=>e in t?c(t,e,{enumerable:!0,configurable:!0,writable:!0,value:i}):t[e]=i;var _=(t,e,i)=>(g(t,typeof e!="symbol"?e+"":e,i),i);const b=""+new URL("../assets/0.5da5d42d.jpg",import.meta.url).href,p=""+new URL("../assets/1.28990496.jpg",import.meta.url).href,m=""+new URL("../assets/0.b9b05c6f.jpg",import.meta.url).href,d=""+new URL("../assets/IMG_20250619_194436745.7e25a322.jpg",import.meta.url).href,h=""+new URL("../assets/IMG_20250619_194454063.cabc3aff.jpg",import.meta.url).href,f=""+new URL("../assets/IMG_20250619_194504317.888c2a25.jpg",import.meta.url).href,j=""+new URL("../assets/IMG_20250619_194508016.492a8348.jpg",import.meta.url).href,u=""+new URL("../assets/0.b7bad842.jpg",import.meta.url).href,v=""+new URL("../assets/IMG_20250613_154442483.b37e2e92.jpg",import.meta.url).href,w=""+new URL("../assets/IMG_20250614_120601545.385ef6da.jpg",import.meta.url).href,R=""+new URL("../assets/IMG_20250616_193816154.f6563e76.jpg",import.meta.url).href,L=""+new URL("../assets/IMG_20250620_123711005.6948b170.jpg",import.meta.url).href,k=""+new URL("../assets/0.da800fb1.jpg",import.meta.url).href,U=""+new URL("../assets/1.ac00c3d3.jpg",import.meta.url).href,M=""+new URL("../assets/0.8f3ab4f2.jpg",import.meta.url).href,I=""+new URL("../assets/0.ea96374d.jpg",import.meta.url).href,G=""+new URL("../assets/1.d8102cfc.jpg",import.meta.url).href,y=""+new URL("../assets/0.82d9c1ad.jpg",import.meta.url).href,D=""+new URL("../assets/1.563ed17c.jpg",import.meta.url).href,W=""+new URL("../assets/2.84fcc8ea.jpg",import.meta.url).href,O=""+new URL("../assets/0.639f986d.jpg",import.meta.url).href,x=""+new URL("../assets/IMG_20250616_130829010_HDR.9e2591ee.jpg",import.meta.url).href,F=""+new URL("../assets/IMG_20250616_130836940_HDR.fd216f03.jpg",import.meta.url).href,B=""+new URL("../assets/IMG_20250616_130840671_HDR.f7a81edd.jpg",import.meta.url).href,H=""+new URL("../assets/IMG_20250616_130848721_HDR.159156a1.jpg",import.meta.url).href,$=""+new URL("../assets/0.407edc7e.jpg",import.meta.url).href,C=""+new URL("../assets/0.3b564c13.jpg",import.meta.url).href,P=""+new URL("../assets/IMG_20250617_122014152.4ac0ca56.jpg",import.meta.url).href,T=""+new URL("../assets/IMG_20250617_122025786.837ac766.jpg",import.meta.url).href,A=""+new URL("../assets/IMG_20250617_122033337.0cd68c57.jpg",import.meta.url).href,S=""+new URL("../assets/0.192cc555.jpg",import.meta.url).href,E=""+new URL("../assets/0.92ce72e6.jpg",import.meta.url).href,V=""+new URL("../assets/1.875816e3.jpg",import.meta.url).href,Y=""+new URL("../assets/0-old.51eae74b.jpg",import.meta.url).href,q=""+new URL("../assets/0.e3888cdf.jpg",import.meta.url).href,z=""+new URL("../assets/1.f88da1a8.jpg",import.meta.url).href,J=""+new URL("../assets/3.b312380d.jpg",import.meta.url).href,K=""+new URL("../assets/4.d7ec8695.jpg",import.meta.url).href,N=""+new URL("../assets/5.b6c65f8c.jpg",import.meta.url).href,Q=""+new URL("../assets/0.2c6c1f4d.jpg",import.meta.url).href,X=""+new URL("../assets/1.c047f711.jpg",import.meta.url).href,Z=""+new URL("../assets/0.d9c1893c.jpg",import.meta.url).href,ee=""+new URL("../assets/1.8b0bf56c.jpg",import.meta.url).href,se=""+new URL("../assets/2.71df6b8f.jpg",import.meta.url).href,te=""+new URL("../assets/3.4c380113.jpg",import.meta.url).href,ie=""+new URL("../assets/4.ce1d1578.jpg",import.meta.url).href,_e=""+new URL("../assets/5.ed674dac.jpg",import.meta.url).href,ae=""+new URL("../assets/6.26d97258.jpg",import.meta.url).href,re=""+new URL("../assets/7.3964dd88.jpg",import.meta.url).href,le=""+new URL("../assets/8.b664dc1f.jpg",import.meta.url).href,ne=""+new URL("../assets/0.4108952c.jpg",import.meta.url).href,oe=""+new URL("../assets/1.0aff6232.jpg",import.meta.url).href,ce=""+new URL("../assets/0.a816a6d4.jpg",import.meta.url).href,ge=""+new URL("../assets/0.eae41a1d.jpg",import.meta.url).href,be=""+new URL("../assets/IMG_20240105_114945923.71e8021c.jpg",import.meta.url).href,pe=""+new URL("../assets/IMG_20240105_114946941.908c4305.jpg",import.meta.url).href,me=""+new URL("../assets/IMG_20240105_114949346.47beb45a.jpg",import.meta.url).href,de=""+new URL("../assets/0.df355d12.jpg",import.meta.url).href,he=""+new URL("../assets/1.601aeeb1.jpg",import.meta.url).href,fe=""+new URL("../assets/2.c9d3c826.jpg",import.meta.url).href,je=""+new URL("../assets/3.546bc532.jpg",import.meta.url).href,ue=""+new URL("../assets/4.f095211a.jpg",import.meta.url).href,ve=`Wood, Hackberry
+Date, 05/25
+Finish, Linseed Oil
+
+`,we=`Wood, Cherry 
+Date, 06/25
+Finish, Danish Oil
+
+`,Re="",Le=`Wood,Cherry
+Date,12/24
+Finish,Danish Oil
+`,ke=`Wood, Red Oak 
+Date, 05/25
+Finish, Wipe on Poly
+
+`,Ue=`Wood, Red Oak & Hackberry
+Date, 05/25
+Finish, Danish Oil 
+
+`,Me=`Wood, Walnut, Cherry, and Hackberry 
+Date, 06/25
+Finish, Linseed Oil
+`,Ie=`Wood, Hackberry 
+Date, 12/24
+Finish, Old Teak Oil
+`,Ge=`Wood, Oak
+Date, ???
+Finish, Rubio Mono Coat 
+
+`,ye=`Wood, Maple
+Date, ???
+Finish, Oil Based Poly
+`,De=`Wood, Red Oak
+Date, 05/23
+Finish, Oak Rubio Mono Coat
+
+
+`,We="",Oe=`Wood, Some Pine I found at the Cabin
+Date, 05/25
+Finish, Wipe on Poly
+`,xe=`Overall Height,87"
+Overall Width,44"
+Length,118"
+Track to Blade Length,65"
+Track Length,54"
+Weight,3750 LB (1700kg)"
+Engine Brand,Briggs and Stratton
+Engine Power,13.5 HP 10 kW
+Fuel,Gasoline
+Hydraulic Flow,7.1 GPM (27 L/min)
+Auxiliary Hydraulics,Yes
+Break Out Force,3257 lbf
+Digging Radius,138"
+Digging Depth,75"
+Backfill Blade Width,44"
+Standard Bucket Width,16"`,Fe=`Wood, Red Oak
+Date, 05/25
+Finish, Wipe on Poly 
+
+`,Be=`Wood,Red Oak Flooring
+Date,?/23
+Finish,Wipe on Poly
+`,He=`Wood, Red Oak
+Date, 05/25
+Finish, Oak Rubio Mono Coat
+
+`,$e=`Wood, Walnut
+Date, 05/25
+Finish, Linseed Oil
+`,r=Object.assign({"/src/lib/images/basket-stand/0.jpg":b,"/src/lib/images/basket-stand/1.jpg":p,"/src/lib/images/bens-table/0.jpg":m,"/src/lib/images/bens-table/IMG_20250619_194436745.jpg":d,"/src/lib/images/bens-table/IMG_20250619_194454063.jpg":h,"/src/lib/images/bens-table/IMG_20250619_194504317.jpg":f,"/src/lib/images/bens-table/IMG_20250619_194508016.jpg":j,"/src/lib/images/catan/0.jpg":u,"/src/lib/images/catan/IMG_20250613_154442483.jpg":v,"/src/lib/images/catan/IMG_20250614_120601545.jpg":w,"/src/lib/images/catan/IMG_20250616_193816154.jpg":R,"/src/lib/images/catan/IMG_20250620_123711005.jpg":L,"/src/lib/images/cherry-bench/0.jpg":k,"/src/lib/images/cherry-bench/1.jpg":U,"/src/lib/images/desk/0.jpg":M,"/src/lib/images/end-table/0.jpg":I,"/src/lib/images/end-table/1.jpg":G,"/src/lib/images/first-box/0.jpg":y,"/src/lib/images/first-box/1.jpg":D,"/src/lib/images/first-box/2.jpg":W,"/src/lib/images/first-cajon/0.jpg":O,"/src/lib/images/first-cajon/IMG_20250616_130829010_HDR.jpg":x,"/src/lib/images/first-cajon/IMG_20250616_130836940_HDR.jpg":F,"/src/lib/images/first-cajon/IMG_20250616_130840671_HDR.jpg":B,"/src/lib/images/first-cajon/IMG_20250616_130848721_HDR.jpg":H,"/src/lib/images/first-live-edge/0.jpg":$,"/src/lib/images/guitar-holders/0.jpg":C,"/src/lib/images/guitar-holders/IMG_20250617_122014152.jpg":P,"/src/lib/images/guitar-holders/IMG_20250617_122025786.jpg":T,"/src/lib/images/guitar-holders/IMG_20250617_122033337.jpg":A,"/src/lib/images/island/0.jpg":S,"/src/lib/images/megs-bench/0.jpg":E,"/src/lib/images/megs-bench/1.jpg":V,"/src/lib/images/milling/0-old.jpg":Y,"/src/lib/images/milling/0.jpg":q,"/src/lib/images/milling/1.jpg":z,"/src/lib/images/milling/3.jpg":J,"/src/lib/images/milling/4.jpg":K,"/src/lib/images/milling/5.jpg":N,"/src/lib/images/pine-bench/0.jpg":Q,"/src/lib/images/pine-bench/1.jpg":X,"/src/lib/images/se17-mini-excavator/0.jpg":Z,"/src/lib/images/se17-mini-excavator/1.jpg":ee,"/src/lib/images/se17-mini-excavator/2.jpg":se,"/src/lib/images/se17-mini-excavator/3.jpg":te,"/src/lib/images/se17-mini-excavator/4.jpg":ie,"/src/lib/images/se17-mini-excavator/5.jpg":_e,"/src/lib/images/se17-mini-excavator/6.jpg":ae,"/src/lib/images/se17-mini-excavator/7.jpg":re,"/src/lib/images/se17-mini-excavator/8.jpg":le,"/src/lib/images/small-bookself/0.jpg":ne,"/src/lib/images/small-bookself/1.jpg":oe,"/src/lib/images/table/0.jpg":ce,"/src/lib/images/tv-stand/0.jpg":ge,"/src/lib/images/tv-stand/IMG_20240105_114945923.jpg":be,"/src/lib/images/tv-stand/IMG_20240105_114946941.jpg":pe,"/src/lib/images/tv-stand/IMG_20240105_114949346.jpg":me,"/src/lib/images/walnut-bookself/0.jpg":de,"/src/lib/images/walnut-bookself/1.jpg":he,"/src/lib/images/walnut-bookself/2.jpg":fe,"/src/lib/images/walnut-bookself/3.jpg":je,"/src/lib/images/walnut-bookself/4.jpg":ue}),Ce=Object.assign({"/src/lib/table_data/basket-stand.csv":ve,"/src/lib/table_data/bens-table.csv":we,"/src/lib/table_data/catan.csv":Re,"/src/lib/table_data/cherry-bench.csv":Le,"/src/lib/table_data/end-table.csv":ke,"/src/lib/table_data/first-box.csv":Ue,"/src/lib/table_data/first-cajon.csv":Me,"/src/lib/table_data/first-live-edge.csv":Ie,"/src/lib/table_data/guitar-holders.csv":Ge,"/src/lib/table_data/island.csv":ye,"/src/lib/table_data/megs-bench.csv":De,"/src/lib/table_data/milling.csv":We,"/src/lib/table_data/pine-bench.csv":Oe,"/src/lib/table_data/se17-mini-excavator.csv":xe,"/src/lib/table_data/small-bookself.csv":Fe,"/src/lib/table_data/table.csv":Be,"/src/lib/table_data/tv-stand.csv":He,"/src/lib/table_data/walnut-bookself.csv":$e});class Pe{constructor(e){_(this,"title");_(this,"slug");_(this,"price");_(this,"description");_(this,"stats");_(this,"image");_(this,"images");_(this,"stripe_link");_(this,"price_id");this.title=e.title,this.slug=e.slug,this.price=e.price,this.description=e.description,this.stats=e.stats,this.image=r[`/src/lib/images/${this.slug}/0.jpg`],this.images=[];for(const i in r)i.startsWith(`/src/lib/images/${this.slug}/`)&&!i.endsWith("/0.jpg")&&this.images.push(r[i]);this.stripe_link=e.stripe_link,this.price_id=e.price_id}}const Te=[{title:"Dinning Room Table",slug:"table",description:"This is a very large custom made dinning room table.",stats:s("table"),price:"$999.99",stripe_link:"",price_id:""},{title:"Walnut Bookself",slug:"walnut-bookself",description:"Live edge walnut bookself.",stats:s("walnut-bookself"),price:"$499.99",stripe_link:"",price_id:""},{title:"Cherry Bench",slug:"cherry-bench",description:"A cherry bench that is made wood from our yard.",stats:s("cherry-bench"),price:"$299.99",stripe_link:"",price_id:""},{title:"Basket Stand",slug:"basket-stand",description:"A cherry bench that is made wood from our yard.",stats:s("basket-stand"),price:"$299.99",stripe_link:"",price_id:""},{title:"Live Edge Coffee Table",slug:"first-live-edge",description:"A cherry bench that is made wood from our yard.",stats:s("first-live-edge"),price:"$299.99",stripe_link:"",price_id:""},{title:"Small End Table",slug:"small-bookself",description:"A cherry bench that is made wood from our yard.",stats:s("small-bookself"),price:"$299.99",stripe_link:"",price_id:""},{title:"Medium End Table",slug:"end-table",description:"A cherry bench that is made wood from our yard.",stats:s("end-table"),price:"$299.99",stripe_link:"",price_id:""},{title:"Island",slug:"island",description:"A cherry bench that is made wood from our yard.",stats:s("island"),price:"$299.99",stripe_link:"",price_id:""},{title:"Pine Bench",slug:"pine-bench",description:"A cherry bench that is made wood from our yard.",stats:s("pine-bench"),price:"$299.99",stripe_link:"",price_id:""},{title:"First Box",slug:"first-box",description:"The first box I made.",stats:s("first-box"),price:"$299.99",stripe_link:"",price_id:""},{title:"Meg's Bench",slug:"megs-bench",description:"A bench I made for Meg.",stats:s("megs-bench"),price:"$299.99",stripe_link:"",price_id:""},{title:"Milling",slug:"milling",description:"",stats:s("milling"),price:"$299.99",stripe_link:"",price_id:""},{title:"TV Stand",slug:"tv-stand",description:"",stats:s("tv-stand"),price:"$299.99",stripe_link:"",price_id:""},{title:"First Cajon",slug:"first-cajon",description:"",stats:s("first-cajon"),price:"",stripe_link:"",price_id:""},{title:"Catan Boardgame",slug:"catan",description:"",stats:s("catan"),price:"",stripe_link:"",price_id:""},{title:"Guitar Holders",slug:"guitar-holders",description:"",stats:s("guitar-holders"),price:"",stripe_link:"",price_id:""},{title:"Ben's First Table",slug:"bens-table",description:"",stats:s("bens-table"),price:"",stripe_link:"",price_id:""}];function s(t){const e=new Map;let l=Ce[`/src/lib/table_data/${t}.csv`].split(`
+`);for(let a=0;a<l.length;a++){const[n,o]=l[a].split(",");e.set(n,o)}return e}const Ae=[],Se=new Map;for(const t of Te){const e=new Pe(t);Ae.push(e),Se.set(e.slug,e)}export{Se as a,Ae as p};
